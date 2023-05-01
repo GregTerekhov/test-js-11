8bd3669b5d81566071a0e2d7bfb22b9643950345
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2023-05-01 17:06:50 +0300

    last commit

---
+++ src/js/api-service.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+
+const API_KEY = '35686876-5d6c301705e2d3bbb33c976fd';
+axios.defaults.baseURL = 'https://pixabay.com/api';
+
+export class ApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async getImages() {
+    const params = new URLSearchParams({
+      key: API_KEY,
+      q: this.searchQuery,
+      page: this.page,
+      image_type: 'photo',
+      orientaion: 'horizontal',
+      per_page: 40,
+    });
+
+    try {
+      // if (response.status !== 200) {
+      //   throw new Error(error, 'Unable to get images');
+      // }
+      const {
+        data: { hits, totalHits },
+      } = await axios.get('/', { params });
+
+      return { hits, totalHits };
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  enlargementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+}
---
+++ src/js/template.js
@@ -0,0 +1,30 @@
+import { refs } from './dom-refs';
+
+export function imagesTemplate(hits) {
+  const markup = hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+
+            <a href="${largeImageURL}">
+              <img src="${webformatURL}" alt="${tags}" width="320" height="240"/></a>
+              <div class="info">
+                <p class="info-item"><b>Likes:</b> ${likes}</p>
+                <p class="info-item"><b>Views:</b> ${views}</p>
+                <p class="info-item"><b>Comments:</b> ${comments}</p>
+                <p class="info-item"><b>Downloads:</b> ${downloads}</p>
+              </div>
+            </div>`;
+      }
+    )
+    .join('');
+  refs.galleryEl.insertAdjacentHTML('beforeend', markup);
+}
