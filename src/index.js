e90e4a4b67fa4b090d1d912764c1b2bc03309ef6
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2023-05-01 17:04:01 +0300

    last commit

---
+++ src/index.js
@@ -0,0 +1,81 @@
+import { Notify } from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import { ApiService } from './js/api-service';
+import { imagesTemplate } from './js/template';
+import { refs } from './js/dom-refs';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 500,
+  captionsData: 'alt',
+});
+
+const apiService = new ApiService();
+
+refs.formEl.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  apiService.query = e.currentTarget.elements.searchQuery.value.trim();
+  apiService.resetPage();
+  clearGallery();
+  if (!apiService.query) {
+    return Notify.failure('Please enter a search word.');
+  }
+  renderImages();
+}
+
+function checkInputQuery(totalHits) {
+  if (apiService.query === '') {
+    return Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+  Notify.success(`Hooray! We found ${totalHits} images.`);
+}
+
+function renderImages() {
+  infiniteScroll.unobserve(refs.observableEl);
+
+  apiService.getImages().then(({ hits, totalHits }) => {
+    if (apiService.page === 1) {
+      checkInputQuery(totalHits);
+    }
+
+    imagesTemplate(hits);
+    lightbox.refresh();
+    infiniteScroll.observe(refs.observableEl);
+
+    if (apiService.page === Math.ceil(totalHits / 40)) {
+      infiniteScroll.unobserve(refs.observableEl);
+      lightbox.refresh();
+      return Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+    apiService.enlargementPage();
+  });
+}
+
+function clearGallery() {
+  refs.galleryEl.innerHTML = '';
+}
+
+const options = {
+  rootMargin: '400px',
+  history: false,
+};
+
+const onEntry = entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting && apiService.query !== '') {
+      if (apiService.page === 1) return;
+
+      renderImages();
+    }
+  });
+};
+
+const infiniteScroll = new IntersectionObserver(onEntry, options);
+infiniteScroll.observe(refs.observableEl);
